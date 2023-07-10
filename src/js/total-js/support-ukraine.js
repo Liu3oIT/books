3ab1e8fc6d07e33eacfcec4f3656b3f4c23ee822
Liu3oIT <liutyivova995@gmail.com>
2023-07-10 22:49:30 +0300

    update

--- src/js/total-js/support-ukraine.js
+++ src/js/total-js/support-ukraine.js
@@ -1,6 +1,7 @@
 import supportUkraineList from '../total-js/support-ukraine-list';
 
 const supportUkraineListEl = document.querySelector('.support-list');
+console.log(supportUkraineListEl);
 const downBtn = document.querySelector('.support-down-btn');
 
 renderCharityBar(supportUkraineList);
