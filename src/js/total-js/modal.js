9953449f7ca6d649bed52bf181047595ddb9684c
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-07-11 20:18:29 +0300

    js-lok

--- src/js/total-js/modal.js
+++ src/js/total-js/modal.js
@@ -81,7 +81,6 @@ import amazon from '../../img/amazon.svg';
 import applebook from '../../img/book-apple.svg';
 import bookshop from '../../img/book-shop.svg';
 
-
 const backDrop = document.querySelector('#book-modal');
 const bestSellerRef = document.querySelector('.books-container');
 const categoriesRef = document.querySelector('.category-books-list');
@@ -93,40 +92,46 @@ const removeNotification = document.querySelector(
 const addNotification = document.querySelector('.modal__add-notification');
 const notification = document.querySelector('.modal__notification');
 let modalContent = document.querySelector('.modal__content');
-let idToLocaleStorege = null;
-let arrToLocaleStoreg = {};
+let idToLocaleStorage = null;
+let arrToLocaleStorage = [];
+
 addBookBtn.addEventListener('click', onAddBookClick);
 closeBtn.addEventListener('click', onBtnCloseClick);
 bestSellerRef.addEventListener('click', onCardClick);
+
 function fetchCategory(id) {
   return fetch(`https://books-backend.p.goit.global/books/${id}`).then(res =>
     res.json()
   );
 }
+
 function renderTargetCategory(id) {
   const markup = `
-      <div class="modal__img-container">
-        <img src="${id.book_image}" alt="${id.title}" class="modal__img">
-      </div>
-      <div class="modal__desc">
-        <h2 class="modal__title">${id.title}</h2>
-        <p class="modal__author">${id.author}</p>
-        <p class="modal__book-desc">${id.description}</p>
-        <ul class="modal__list">
-          <li class="modal__item"><a href="${id.buy_links[0].url}" class="amazon-link"><img class="store-link-img amazon-img" src="${amazon}" alt=""></a></li>
-          <li class="modal__item"><a href="${id.buy_links[1].url}" class="app-book-link"><img class="store-link-img" src="${applebook}" alt=""></a></li>
-          <li class="modal__item"><a href="${id.buy_links[3].url}" class="book-shop-link"><img class="store-link-img" src="${bookshop}" alt=""></a></li>
-        </ul>
-</div>
-    `;
+    <div class="modal__img-container">
+      <img src="${id.book_image}" alt="${id.title}" class="modal__img">
+    </div>
+    <div class="modal__desc">
+      <h2 class="modal__title">${id.title}</h2>
+      <p class="modal__author">${id.author}</p>
+      <p class="modal__book-desc">${id.description}</p>
+      <ul class="modal__list">
+        <li class="modal__item"><a href="${id.buy_links[0].url}" class="amazon-link"><img class="store-link-img amazon-img" src="${amazon}" alt=""></a></li>
+        <li class="modal__item"><a href="${id.buy_links[1].url}" class="app-book-link"><img class="store-link-img" src="${applebook}" alt=""></a></li>
+        <li class="modal__item"><a href="${id.buy_links[3].url}" class="book-shop-link"><img class="store-link-img" src="${bookshop}" alt=""></a></li>
+      </ul>
+    </div>
+  `;
+
   modalContent.innerHTML = '';
   modalContent.innerHTML = markup;
 }
+
 function getCategory(id) {
   fetchCategory(id).then(res => {
     renderTargetCategory(res);
   });
 }
+
 function onBtnCloseClick(e) {
   if (e.code === 'Escape') {
     backDrop.removeEventListener('keydown', onBtnCloseClick);
@@ -139,6 +144,7 @@ function onBtnCloseClick(e) {
     backDrop.classList.add('is-hidden');
   }
 }
+
 function onCardClick(e) {
   const card = e.target;
   const el = card.closest('[data-id]');
@@ -149,21 +155,27 @@ function onCardClick(e) {
   if (card.classList.contains('books-btn')) {
     return;
   }
-  idToLocaleStorege = id;
+  idToLocaleStorage = id;
   getCategory(id);
   backDrop.classList.remove('is-hidden');
 }
+
 function onAddBookClick(res) {
   notification.classList.toggle('hidden');
   removeNotification.classList.toggle('hidden');
   addNotification.classList.toggle('hidden');
+
   if (addNotification.classList.contains('hidden')) {
-    arrToLocaleStoreg.push(idToLocaleStorege);
-    localStorage.setItem('shopping-list', JSON.stringify(arrToLocaleStoreg));
+    const index = arrToLocaleStorage.indexOf(idToLocaleStorage);
+    if (index === -1) {
+      arrToLocaleStorage.push(idToLocaleStorage);
+      localStorage.setItem('shopping-list', JSON.stringify(arrToLocaleStorage));
+    }
   }
+
   if (removeNotification.classList.contains('hidden')) {
-    const arrTofilter = JSON.parse(localStorage.getItem('shopping-list'));
-    const filtredArr = arrTofilter.filter(id => id !== idToLocaleStorege);
-    localStorage.setItem('shopping-list', JSON.stringify(filtredArr));
+    const arrToFilter = JSON.parse(localStorage.getItem('shopping-list'));
+    const filteredArr = arrToFilter.filter(id => id !== idToLocaleStorage);
+    localStorage.setItem('shopping-list', JSON.stringify(filteredArr));
   }
 }
