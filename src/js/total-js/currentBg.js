5f8f73a92e439781bf3a924796dccf76d79b891b
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-07-12 00:48:08 +0200

    added icons, some css

--- src/js/total-js/currentBg.js
+++
@@ -1,7 +0,0 @@
-const currentHomePage = document.querySelector('.header-link-home');
-
-currentHomePage.classList.add('current');
-
-const currentShoppingPage = document.querySelector('.header-shopping-link');
-
-currentShoppingPage.classList.add('current');
