23624af14e86143e909fa01444d986a6043bbd96
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-07-12 17:08:34 +0200

    edit function

--- src/js/total-js/header.js
+++ src/js/total-js/header.js
@@ -4,7 +4,9 @@ const checkBoxEl = document.querySelector('#checkbox');
 const body = document.querySelector('body');
 
 if (checkBoxEl) {
-    checkBoxEl.addEventListener('change', (event) => { onCheckboxClickHandler(event, body) });
+  checkBoxEl.addEventListener('change', event => {
+    onCheckboxClickHandler(event, body);
+  });
 }
 
 savedThemeOnReloaded(body, checkBoxEl);
@@ -12,11 +14,9 @@ savedThemeOnReloaded(body, checkBoxEl);
 const currentHomePage = document.querySelector('.home-btn');
 const currentShoppingPage = document.querySelector('.list-btn');
 
-    if (currentShoppingPage) {
-    if (window.location.href === currentShoppingPage.href) {
+if (currentShoppingPage) {
+  if (window.location.href === currentShoppingPage.href) {
     currentShoppingPage.classList.add('current');
-    // mobileMenuList.add('current');
     currentHomePage.classList.remove('current');
-    // mobileMenuHome.classList.remove('current');
-}
+  }
 }
