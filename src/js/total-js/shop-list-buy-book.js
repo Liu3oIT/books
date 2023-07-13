ac6bf110fb091bf56bfd425f8d59998f41191eab
Liu3oIT <liutyivova995@gmail.com>
2023-07-13 16:24:19 +0300

    fix-shop-cart

--- src/js/total-js/shop-list-buy-book.js
+++ src/js/total-js/shop-list-buy-book.js
@@ -38,14 +38,14 @@ const processBook = book => {
         <div class="sh-book-info-link-wrap">
           <p class="sh-book-author">${book.author}</p>
           <ul class="shopping-links">
-            <li class="sh-soc-item"><a href="${book.buy_links[0].url}" class="amazon-link" target="_blank"><img class="store-link-img amazon-img" src="${amazon}" alt=""></a></li>
-            <li class="sh-soc-item"><a href="${book.buy_links[1].url}" class="app-book-link" target="_blank"><img class="store-link-img" src="${applebook}" alt=""></a></li>
-            <li class="sh-soc-item"><a href="${book.buy_links[3].url}" class="book-shop-link" target="_blank"><img class="store-link-img" src="${bookshop}" alt=""></a></li>
+            <li class="sh-soc-item"><a href="${book.buy_links[0].url}" class="amazon-link" target="_blank"><img class="store-link-img amazon-store" src="${amazon}" alt=""></a></li>
+            <li class="sh-soc-item"><a href="${book.buy_links[1].url}" class="app-book-link" target="_blank"><img class="store-link-img apple-store" src="${applebook}" alt=""></a></li>
+            <li class="sh-soc-item"><a href="${book.buy_links[3].url}" class="book-shop-link" target="_blank"><img class="store-link-img book-shop" src="${bookshop}" alt=""></a></li>
           </ul>
         </div>
       </div>
       <button class="sh-list-delete-btn" type="button">
-        <svg class="sh-list-delete-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+        <svg class="sh-list-delete-icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="-2 -2 24 24">
           <path d="M7.25 1.75h5.5m-11 2.75h16.5m-1.833 0-.643 9.643c-.097 1.446-.145 2.17-.457 2.718a2.75 2.75 0 0 1-1.19 1.114c-.569.275-1.293.275-2.743.275H8.616c-1.45 0-2.174 0-2.743-.275a2.75 2.75 0 0 1-1.19-1.114c-.312-.548-.36-1.272-.457-2.718L3.583 4.5m4.584 4.125v4.583m3.666-4.583v4.583" />
         </svg>
       </button>
