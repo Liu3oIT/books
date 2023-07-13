24ad0fa55f8d6113518673727a127d6c5cfe86aa
Liza <lizkapylypchuk123456@gmail.com>
2023-07-13 16:48:10 +0300

    xx

--- src/js/total-js/API.js
+++ src/js/total-js/API.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
-
+import {showLoader,hideLoader} from './loader';
 export default class NewApiService {
   constructor() {}
 
   async fetchCategoryBooks() {
+    showLoader()
     const BASE_URL = 'https://books-backend.p.goit.global/books/category-list';
     try {
       let axisoInstance = axios.create();
@@ -17,10 +18,13 @@ export default class NewApiService {
       Notiflix.Notify.warning(
         `Oops! Something went wrong. You caught the following error: ${error.message}.`
       );
+    } finally{
+      hideLoader()
     }
   }
 
   async fetchTopFiveBooks() {
+    showLoader()
     const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
     try {
       let axisoInstance = axios.create();
@@ -33,6 +37,8 @@ export default class NewApiService {
       Notiflix.Notify.warning(
         `Oops! Something went wrong. You caught the following error: ${error.message}.`
       );
+    }  finally{
+      hideLoader()
     }
   }
   async fetchBooksForId(id) {
